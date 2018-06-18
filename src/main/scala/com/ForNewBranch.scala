a8d1142ef6cdefea2156d30409b7bb7d83144aea
Prabhakarg1 <32515781+Prabhakarg1@users.noreply.github.com>
2018-06-18 22:28:56 +0530

    update for newBranch

--- src/main/scala/com/ForNewBranch.scala
+++ src/main/scala/com/ForNewBranch.scala
@@ -2,7 +2,8 @@ package com
 
 object ForNewBranch {
   def main(args:Array[String]):Unit={
-    println("sequence of 10 chars"+ Seq(1 to 10))
+    println("sequence of 10 chars"+ Seq(1 to 10));
+    prinln("added from git")
   }
 
 
