50831c55983e28b6dd7a2e2c9ddde8699eadd85a
Prabhakar Gurram <PG00560294@techmahindra.com>
2018-06-18 13:01:27 +0530

    first commit

--- src/main/scala/com/ScalaTestExample.scala
+++ src/main/scala/com/ScalaTestExample.scala
@@ -5,6 +5,7 @@ class ScalaTestExample {
   def stringRevers(givenString: String): Unit = {
     try {
       println(s"Using revers function : $givenString.reverse")
+      println("created new local branch")
       var builder: StringBuilder = StringBuilder.newBuilder;
       for (i <- givenString.size-1 to 0 by -1) {
         val chr = givenString.charAt(i.toInt)
